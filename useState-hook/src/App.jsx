0e75e0c62c9286dc32a713d89b24c587b0c6485f
samarth-mathur12 <samarthmathur199@gmail.com>
2025-07-05 08:19:45 +0530

    useState hook

---
+++ useState-hook/src/App.jsx
@@ -0,0 +1,21 @@
+// React hook = Special function that allows functional components to use
+//              React features without writting class components(React v16.8)
+//               (useState, useEffect, useContext, useReducer, useCallback and more...)
+
+// useState() = A React hook that allows the creation of a stateful variable
+//              AND a setter function to update its value in the virtual DOM.
+//              [name, setName]
+
+import Counter from './Counter.jsx'
+function App() {
+  
+  return (
+    <>
+      <Counter/>
+
+      
+    </>
+  )
+}
+
+export default App
